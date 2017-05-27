a32c200e4963a607e2431e97dd8da30446e4ab36
Joren Engbers <joren.engbers@gmail.com>
2017-05-27 21:05:10 +0200

    Replaced Cardboard VR for a custom made solution
    Due to the amount of problems using Cardboard VR, I designed a custom made solution using two camera's parented to the main camera and a gyroscope script with support for iOS & Android.

---
+++ FATGemaakt - Virtual Reality App/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.orientation = ScreenOrientation.Portrait;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void openVR(){
+		Application.LoadLevel("VR-interface-demo");
+	}
+
+
+
+
+}
