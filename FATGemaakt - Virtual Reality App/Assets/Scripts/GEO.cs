da600c9f8e48f888dc46602ad9b5004876c07f15
Joren Engbers <joren.engbers@gmail.com>
2017-05-28 10:44:07 +0200

    GPS functionaliteit toegevoegd aan Sonar scene
    Sonar scene haalt coordinaten van gebruiker op en geeft ze weer in een text label.

---
+++ FATGemaakt - Virtual Reality App/Assets/Scripts/GEO.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GEO : MonoBehaviour
+{
+
+	public static GEO Instance{set; get;}
+	public float latitude;
+	public float longitude;
+
+	private void Start(){
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+		StartCoroutine (StartLocationService());
+
+	}
+
+	private IEnumerator StartLocationService(){
+		if(!Input.location.isEnabledByUser){
+			Debug.Log ("User has not enabled GPS");
+			yield break;
+		}
+
+		Input.location.Start();
+		int maxWait = 20;
+		while(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0){
+			yield return new WaitForSeconds(1);
+			maxWait--;
+
+
+		}
+
+		if(maxWait <= 0){
+			Debug.Log("Timed out");
+			yield break;
+		}
+
+		if(Input.location.status == LocationServiceStatus.Failed){
+			Debug.Log("Unable to determin device location");
+			yield break;
+		}
+		Debug.Log("Gettinghere");
+
+		latitude = Input.location.lastData.latitude;
+		longitude = Input.location.lastData.longitude;
+
+		yield break;
+	}
+
+
+}
---
+++ FATGemaakt - Virtual Reality App/Assets/Scripts/UpdateGPSText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour {
+
+	public Text coordinates;
+
+//	void Start(){
+//		coordinates = GetComponent<Text>();
+//		}
+
+	private void Update(){
+		coordinates.text = "Lat" + GEO.Instance.latitude.ToString() + "  lon:" + GEO.Instance.longitude.ToString();
+	}
+}
